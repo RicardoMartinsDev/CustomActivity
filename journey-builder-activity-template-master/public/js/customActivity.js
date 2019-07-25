7f369ee080fa3459c4a267a27ebffa9c47faad84
SACRicardoMartins <ricardo.martins@stepahead.pt>
2019-07-25 11:54:15 +0100

    de
    de

--- journey-builder-activity-template-master/public/js/customActivity.js
+++ journey-builder-activity-template-master/public/js/customActivity.js
@@ -67,7 +67,7 @@ define([
     }
 
     function save() {
-       /* var postcardURLValue = $('#postcard-url').val();
+        var postcardURLValue = $('#postcard-url').val();
         var postcardTextValue = $('#postcard-text').val();
 
         payload['arguments'].execute.inArguments = [{
