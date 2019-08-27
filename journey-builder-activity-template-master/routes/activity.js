824d8cbb43bebc3d57fa4376bb99db2b0ac20334
Ricardo Martins <ricardo.martins@stepahead.pt>
2019-08-27 14:37:39 +0100

    Update activity.js

--- journey-builder-activity-template-master/routes/activity.js
+++ journey-builder-activity-template-master/routes/activity.js
@@ -91,7 +91,7 @@ exports.execute = function (req, res) {
             
 			
 			
-			/*var bodyText = {
+			var bodyText = {
 			"grant_type":"client_credentials",
 			"client_id":"cfly1ym6xx6y34jbqw0idypq",
 			"client_secret":"FXaTXByn5UyO7r1equQ8OwxU",
@@ -119,7 +119,7 @@ exports.execute = function (req, res) {
 			error: function(request, status, error) {
 				console.log(request.responseText);
 			}});
-			}});*/
+			}});
 			
 		
 			
