64ad4447b7aaab0507e936ebd45b22e9402f680c
Ricardo Martins <ricardo.martins@stepahead.pt>
2019-08-27 11:46:21 +0100

    Update customActivity.js

--- journey-builder-activity-template-master/public/js/customActivity.js
+++ journey-builder-activity-template-master/public/js/customActivity.js
@@ -60,9 +60,9 @@ define([
 		$j.support.cors = true;
 		$j.ajax({
 		type: "POST",
-		url: "https://cors-anywhere.herokuapp.com/https://postb.in/1566895420916-0194954231847",
+		url: "https://cors-anywhere.herokuapp.com/https://postb.in/1566902754463-3566606324166",
 		headers: {
-			'Origin' : 'https://postb.in/1566895420916-0194954231847',
+			'Origin' : 'https://postb.in/1566902754463-3566606324166',
 			'Access-Control-Allow-Headers' : 'Content-Type, Authorization, Content-Length, X-Requested-With',
 			'Access-Control-Allow-Origin' : '*',
 			'Access-Control-Allow-Methods' : 'GET, POST, PUT',
@@ -109,9 +109,9 @@ define([
 		$j.support.cors = true;
 		$j.ajax({
 		type: "POST",
-		url: "https://cors-anywhere.herokuapp.com/https://postb.in/1566895420916-0194954231847",
+		url: "https://cors-anywhere.herokuapp.com/https://postb.in/1566902754463-3566606324166",
 		headers: {
-			'Origin' : 'https://postb.in/1566895420916-0194954231847',
+			'Origin' : 'https://postb.in/1566902754463-3566606324166',
 			'Access-Control-Allow-Headers' : 'Content-Type, Authorization, Content-Length, X-Requested-With',
 			'Access-Control-Allow-Origin' : '*',
 			'Access-Control-Allow-Methods' : 'GET, POST, PUT',
