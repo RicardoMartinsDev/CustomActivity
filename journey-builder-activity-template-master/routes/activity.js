aa4abf3039046005b92d7465957f65755c154fcc
Ricardo Martins <ricardo.martins@stepahead.pt>
2019-08-27 18:51:40 +0100

    Update activity.js

--- journey-builder-activity-template-master/routes/activity.js
+++ journey-builder-activity-template-master/routes/activity.js
@@ -72,18 +72,9 @@ exports.save = function (req, res) {
  * POST Handler for /execute/ route of Activity.
  */
 exports.execute = function (req, res) {
-
-    // example on how to decode JWT
-    JWT(req.body, process.env.jwtSecret, (err, decoded) => {
-
-        // verification error -> unauthorized request
-        if (err) {
-            console.error(err);
-            return res.status(401).end();
-        }
-		
-		
-		var bodyText = {
+	
+	
+	var bodyText = {
 			"grant_type":"client_credentials",
 			"client_id":"cfly1ym6xx6y34jbqw0idypq",
 			"client_secret":"FXaTXByn5UyO7r1equQ8OwxU",
@@ -111,47 +102,30 @@ exports.execute = function (req, res) {
 			error: function(request, status, error) {
 				console.log(request.responseText);
 			}});
+	
+	
+	
+	
+	
+	
+	
+    // example on how to decode JWT
+    JWT(req.body, process.env.jwtSecret, (err, decoded) => {
+
+        // verification error -> unauthorized request
+        if (err) {
+            console.error(err);
+            return res.status(401).end();
+        }
+		
+		
+		
 			
         if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
             
             // decoded in arguments
             var decodedArgs = decoded.inArguments[0];
             
-			
-			
-			var bodyText = {
-			"grant_type":"client_credentials",
-			"client_id":"cfly1ym6xx6y34jbqw0idypq",
-			"client_secret":"FXaTXByn5UyO7r1equQ8OwxU",
-			"variaveis" : "teste segundo if"
-			};
-			var $j = jQuery.noConflict();
-			var token;
-			$j.support.cors = true;
-			$j.ajax({
-			type: "POST",
-			url: "https://cors-anywhere.herokuapp.com/https://postb.in/1566914112218-9669084278866",
-			headers: {
-				'Origin' : 'https://postb.in/1566914112218-9669084278866',
-				'Access-Control-Allow-Headers' : 'Content-Type, Authorization, Content-Length, X-Requested-With',
-				'Access-Control-Allow-Origin' : '*',
-				'Access-Control-Allow-Methods' : 'GET, POST, PUT',
-				'Content-Type': 'application/json'
-			},
-			crossDomain: true,
-			data: JSON.stringify(bodyText),
-			dataType: 'json',
-			success: function(responseData, status, xhr) {
-				console.log(responseData);
-			},
-			error: function(request, status, error) {
-				console.log(request.responseText);
-			}});
-			
-		
-			
-			
-			
 			
             logData(req);
             res.send(200, 'Execute');
