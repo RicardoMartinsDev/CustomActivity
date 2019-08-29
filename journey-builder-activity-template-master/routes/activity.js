17ef6eda6f57afe9621266c4f626086b4a419940
Ricardo Martins <ricardo.martins@stepahead.pt>
2019-08-29 12:05:02 +0100

    Update activity.js

--- journey-builder-activity-template-master/routes/activity.js
+++ journey-builder-activity-template-master/routes/activity.js
@@ -82,44 +82,7 @@ exports.save = function (req, res) {
 exports.execute = function (req, res) {
 	console.log("teste de log");
 	console.log(process.env.jwtSecret);
-	
-								
-	
-	
-	
-	
-											
-				/*$(document).ready(function () {
-					var bodyText = {
-					"grant_type":"client_credentials",
-					"client_id":"cfly1ym6xx6y34jbqw0idypq",
-					"client_secret":"FXaTXByn5UyO7r1equQ8OwxU",
-					"variaveis" : "teste primeiro if"
-					};
-					var $j = jQuery.noConflict();
-					var token;
-					$j.support.cors = true;
-					$j.ajax({
-					type: "POST",
-					url: process.env.postURL,
-					headers: {
-						'Origin' : process.env.postURL,
-						'Access-Control-Allow-Headers' : 'Content-Type, Authorization, Content-Length, X-Requested-With',
-						'Access-Control-Allow-Origin' : '*',
-						'Access-Control-Allow-Methods' : 'GET, POST, PUT',
-						'Content-Type': 'application/json'
-					},
-					crossDomain: true,
-					data: JSON.stringify(bodyText),
-					dataType: 'json',
-					success: function(responseData, status, xhr) {
-						console.log(responseData);
-					},
-					error: function(request, status, error) {
-						console.log(request.responseText);
-					}});
-				});*/
-	
+		
     // example on how to decode JWT
     JWT(req.body, process.env.jwtSecret, (err, decoded) => {
 
