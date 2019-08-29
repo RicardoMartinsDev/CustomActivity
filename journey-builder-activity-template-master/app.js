5c6cab8633c1e9d27096b6b9b390181f0ca45c43
Ricardo Martins <ricardo.martins@stepahead.pt>
2019-08-29 16:33:18 +0100

    Update app.js

--- journey-builder-activity-template-master/app.js
+++ journey-builder-activity-template-master/app.js
@@ -18,7 +18,7 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.raw({type: 'application/jwt'}));
 app.use(bodyParser.urlencoded({ extended: true }));
-
+ 
 app.use(override);
 app.use(favicon);
 
