cf8dca01d871dd4e18c2a522d3b373f2a6e0b893
Ricardo Martins <ricardo.martins@stepahead.pt>
2019-09-02 14:31:45 +0100

    Update customActivity.js

--- journey-builder-activity-template-master/public/js/customActivity.js
+++ journey-builder-activity-template-master/public/js/customActivity.js
@@ -138,11 +138,11 @@ define([
         payload.name = name;
 
         payload['arguments'].execute.inArguments = [{ 
-		
+				"Email": argumentos
 			/*"Email": "{{Contact.Attribute.Hands_On.Email}}",
 			"Nome": "{{Contact.Attribute.Hands_On.Primeiro_Nome}}",
 			"Apelido": "{{Contact.Attribute.Hands_On.Apelido}}"	*/
-			argumentos
+
 			
 		}];
 			
