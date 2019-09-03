0818d8f24befabeab1d3b17fb029f26cd809ad4a
Ricardo Martins <ricardo.martins@stepahead.pt>
2019-09-03 11:10:57 +0100

    Update activity.js

--- journey-builder-activity-template-master/routes/activity.js
+++ journey-builder-activity-template-master/routes/activity.js
@@ -104,7 +104,7 @@ exports.execute = function (req, res) {
 
 								const options = {
 								  hostname: 'postb.in',
-								  path: '/1567503923866-3534726044163',
+								  path: '/1567505446097-0160199289675',
 								  method: 'POST',
 								  headers: {
 									'Content-Type': 'application/json',
